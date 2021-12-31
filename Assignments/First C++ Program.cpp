5ca8c21d6b58abba7ec495e2714eda03be2e7d5c
tawhidmonowar <tawhidmonowar@gmail.com>
2022-01-01 01:31:21 +0600

    Initial commit

---
+++ Assignments/First C++ Program.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    cout << "C++ is cool";
+    return 0;
+}
---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int myVariables = 10;
+    cout << myVariables;
+
+    return 0;
+}
