b798e468296222e35ab48c0f7a05edf6b23a3bc1
tawhidmonowar <tawhidmonowar@gmail.com>
2022-01-14 15:29:14 +0600

    14-01-2022

--- main.cpp
+++ main.cpp
@@ -1,10 +1,36 @@
 #include <iostream>
+
 using namespace std;
 
 int main()
 {
-    int myVariables = 10;
-    cout << myVariables;
+    int i,n,high,total=0,pay;
+    cin >> n;
+    int x[10000];
+
+    for(i=0;i<n;i++)
+    {
+        cin >> x[i];
+    }
+
+    high = x[0];
+
+    for(i=0;i<n;i++)
+    {
+        if(high<x[i])
+        {
+            high=x[i];
+        }
+    }
+
+    for(i=0;i<n;i++)
+    {
+        total+=x[i];
+    }
+
+    pay = total-(high/2);
+
+    cout << pay;
 
     return 0;
 }
