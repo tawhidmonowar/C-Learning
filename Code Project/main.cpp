50c29d517834d55f30da62a388a03608fe863c35
tawhidmonowar <tawhidmonowar@gmail.com>
2022-02-13 01:28:43 +0600

    13-02-2022

---
+++ Code Project/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "student.h"
+using namespace std;
+
+int main()
+{
+    student tawhid;
+    tawhid.display();
+    return 0;
+}
---
+++ Code Project/src/student.cpp
@@ -0,0 +1,13 @@
+#include "student.h"
+#include <iostream>
+using namespace std;
+
+student::student()
+{
+
+}
+
+void student::display()
+{
+    cout << "Hello World";
+}
